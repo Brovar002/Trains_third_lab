cca72a98deb0e5dacca626d68511edb3d9c0383f
Даниил Гончаров <brovar002@gmail.com>
2020-11-11 19:16:50 +0300

    tr

--- Trains_third_lab/main.cpp
+++ Trains_third_lab/main.cpp
@@ -26,6 +26,105 @@ public:
     string TypeS;
 };
 
+class Way :public Station {
+public:
+    Station Start;
+    Station End;
+    int dWay;
+};
+
+class Train :public Locomotive, public Wagon, public Way {
+public:
+    Way* WayP;
+    Wagon* AllV;
+    int speedp;
+    int Strong;
+    int AllWay;
+    int kolWs;
+    int ostWay;
+    int kolWagonov;
+    int VesIn;
+
+    void Sostav(int n, int g, Way* A, Station* B, int kolS, int kolV) {
+        Strong = 0;
+        int vb;
+        int dw;
+        string v;
+        cout << "введите макс. скорость локомотива" << endl;
+        cin >> speed;
+        cout << "введите срок эксплуатации" << endl;
+        cin >> Moshc;
+        cout << "введите макс. тяговую силу локомотива" << endl;
+        cin >> Mas;
+        AllWay = 0;
+        WayP = new Way[n];
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                cout << "выберите имя начальной точки из возможных" << endl;
+                for (int f = 0; f < kolS; f++) {
+                    cout << B[f].Name << " ";
+                }
+                cout << endl;
+                cin >> v;
+                WayP[i].Start.Name = v;
+                for (int f = 0; f < kolS; f++) {
+                    if (B[f].Name == v) {
+                        WayP[i].Start.TypeS = B[f].TypeS;
+                    }
+                }
+            }
+            else {
+                WayP[i].Start = WayP[i - 1].End;
+            }
+            cout << "выберите имя следующей станции из возможных" << endl;
+            for (int f = 0; f < kolV; f++) {
+                if (A[f].Start.Name == WayP[i].Start.Name) {
+                    cout << A[f].End.Name << " ";
+                }
+            }
+            cout << endl;
+            cin >> v;
+            WayP[i].End.Name = v;
+            for (int f = 0; f < kolS; f++) {
+                if (B[f].Name == v) {
+                    WayP[i].End.TypeS = B[f].TypeS;
+                }
+            }
+            for (int f = 0; f < kolV; f++) {
+                if (A[f].Start.Name == WayP[i].Start.Name) {
+                    if (A[f].End.Name == WayP[i].End.Name) {
+                        WayP[i].dWay = A[f].dWay;
+                    }
+                }
+            }
+            AllWay = AllWay + WayP[i].dWay;
+        }
+        kolWs = n;
+        AllV = new Wagon[g];
+        for (int i = 0; i < g; i++) {
+            cout << "выберите тип вагона:" << " 1-товарный" << " 2-пассажирский" << endl;
+            cin >> vb;
+            if (vb == 1) {
+                AllV[i].TypeV = "tov";
+                Strong++;
+            }
+            else {
+                AllV[i].TypeV = "pas";
+                Strong++;
+            }
+            if (Strong == Mas) {
+                i = g;
+                cout << "досигнуто максимальное число вагонов" << endl;
+            }
+            AllV[i].Gruz = 0;
+        }
+        kolWagonov = g;
+        ostWay = 0;
+        VesIn = 0;
+        speedp = speed - g;
+    }
+};
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
